e42ec3f00ffe1d54468c1c0bd8a4480156fa902e
sur2 <sur2@naver.com>
2020-03-14 00:22:24 +0900

    loop

--- NadoCoding/NadoCoding/loop.c
+++ NadoCoding/NadoCoding/loop.c
@@ -1,7 +1,25 @@
 #include<stdio.h>
 
 // main함수 정의 후 사용
-int main(void)
+int main_loof(void)
 {
+	// 피라미드 쌓기
+	int floor = 0;
+	printf("몇 층으로 쌓겠습니까?\n");
+	scanf_s("%d", &floor);
+	for (int i = 0; i < floor; i++)
+	{
+		for (int j = i; j < floor - 1; j++)
+		{
+			printf(" ");
+		}
+		for (int j = 0; j < i*2+1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+
 	return 0;
 }
