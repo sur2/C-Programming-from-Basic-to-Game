3a415685abb567c7cde456cd02cfd17854fe4c57
sur2 <sur2@naver.com>
2020-04-09 21:59:20 +0900

    다차원 배열

--- NadoCoding/NadoCoding/m_array.c
+++ NadoCoding/NadoCoding/m_array.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 // 10마리의 서로 다른 동물 (각 카드 2장씩)
@@ -8,7 +9,8 @@
 
 
 int arrayAnimal[4][5]; // 카드 지도 (4*5=20장)
-char * strAnimal[10];
+int checkAnimal[4][5]; // 뒤집혔는지 여부 확인
+char* strAnimal[10];
 
 void initAnimalArray();
 void initAnimalName();
@@ -16,15 +18,81 @@ void shuffleAnimal();
 int getEmptyPosition();
 int conv_pos_x(int x);
 int conv_pos_y(int y);
+void printAnimals();
+void printQuestion();
+int foundAllAnimals();
 
 int main(void)
 {
-	srand(time(NULL));
+	srand((unsigned)time(NULL));
 
 	initAnimalArray();
 	initAnimalName();
 
 	shuffleAnimal();
+
+	// 실패 횟수
+	int failCount = 0;
+
+	while (1) {
+		// 사용자가 선택한 첫 번째 수
+		int select1 = 0;
+		// 사용자가 선택한 두 번째 수
+		int select2 = 0;
+
+		// 동물 위치 출력
+		printAnimals();
+		// 문제 출력 (카드 지도)
+		printQuestion();
+		printf("뒤집을 카드를 2개 고르세요");
+		scanf_s("%d %d", &select1, &select2);
+
+		// 같은 카드 선택 시 무효
+		if (select1 == select2)
+		{
+			continue;
+		}
+
+		// 좌표에 해당하는 카드를 뒤집어 보고 같은지 안같은지 확인
+		// 정수 좌표를 (x, y)로 변환
+		int firstSelect_x = conv_pos_x(select1);
+		int firstSelect_y = conv_pos_y(select1);
+
+		int secondSelect_x = conv_pos_x(select2);
+		int secondSelect_y = conv_pos_y(select2);
+
+		// 같은 동물인 경우
+		if (
+			(checkAnimal[firstSelect_x][firstSelect_y] == 0
+				&& checkAnimal[secondSelect_x][secondSelect_y] == 0)
+			&&
+			(arrayAnimal[firstSelect_x][firstSelect_y]
+				== arrayAnimal[secondSelect_x][secondSelect_y])
+			)
+		{
+			printf("\n\n빙고! : %s 발견\n\n", strAnimal[arrayAnimal[firstSelect_x][firstSelect_y]]);
+			checkAnimal[firstSelect_x][firstSelect_y] = 1;
+			checkAnimal[secondSelect_x][secondSelect_y] = 1;
+		}
+		// 다른 동물인 경우
+		else
+		{
+			printf("\n\n 땡!! (틀렸거나, 이미 뒤집힌 카드입니다.)\n");
+			printf("%d : %s\n", select1, strAnimal[arrayAnimal[firstSelect_x][firstSelect_y]]);
+			printf("%d : %s\n", select2, strAnimal[arrayAnimal[secondSelect_x][secondSelect_y]]);
+			printf("\n\n");
+
+			failCount++;
+		}
+
+		// 모든 동물을 찾았는지 여부, 1 : 참, 0 : 거짓
+		if (foundAllAnimals() == 1)
+		{
+			printf("\n\n 축하합니다! 모든 동물을 다 찾았네요. \n");
+			printf("지금까지 총 %d 번 실수하였습니다.\n", failCount);
+			break;
+		}
+	}
 
 	return 0;
 }
@@ -95,4 +163,57 @@ int conv_pos_x(int x)
 int conv_pos_y(int y)
 {
 	return y % 5;
+}
+
+void printAnimals()
+{
+	printf("\n=== 쉿! 비밀인데 몰래 보여드림 ㅋㅋ ===\n\n");
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			printf("%8s", strAnimal[arrayAnimal[i][j]]);
+		}
+		printf("\n");
+	}
+	printf("=======================================\n\n");
+}
+// 문제 출력(카드 지도)
+void printQuestion()
+{
+	printf("\n\n(문제)\n");
+	int seq = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			// 카드를 뒤집어서 정답을 맞췄으면 '동물 이름'
+			if (checkAnimal[i][j] != 0)
+			{
+				printf("%8s", strAnimal[arrayAnimal[i][j]]);
+			}
+			// 아직 뒤집지 못했으면 (정답을 못맞췄으면) 뒷면 -> 위치를 나타내는 숫자
+			else
+			{
+				printf("%8d", seq);
+			}
+			seq++;
+		}
+		printf("\n");
+	}
+}
+
+int foundAllAnimals()
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			if (checkAnimal[i][j] == 0)
+			{
+				return 0;
+			}
+		}
+	}
+	return 1;
 }
